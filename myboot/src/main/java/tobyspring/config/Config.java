b33cfd72039a1407cc388a5318bc318b60025974
shunyang <shunyang@smilegate.com>
2023-04-13 16:21:38 +0900

    Config 클래스 컴포넌트 스캔 대상 제외 @Import 사용해 직접 등록

--- myboot/src/main/java/tobyspring/config/Config.java
+++ myboot/src/main/java/tobyspring/config/Config.java
@@ -1,4 +1,4 @@
-package tobyspring.myboot;
+package tobyspring.config;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -6,6 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.DispatcherServlet;
+
+/*
+ ComponentScan이 붙은 클래스의 패키지가 컴포넌트 스캔 시작 위치가 됨
+ ComponentScan이 MybootApplication에 붙어있으니까 myboot 패키지가 컴포넌트 스캔 시작 위치가 됨
+ config 패키지에 있는 Config 클래스는 컴포넌트 스캔 대상에서 제외됨
+ */
+
 
 @Configuration
 public class Config {
--- myboot/src/main/java/tobyspring/myboot/MySpringBootAnnotation.java
+++ myboot/src/main/java/tobyspring/myboot/MySpringBootAnnotation.java
@@ -2,6 +2,8 @@ package tobyspring.myboot;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import tobyspring.config.Config;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -17,9 +19,14 @@ import java.lang.annotation.Target;
 TYPE : Class + Interface + Enum 이 대상이 됨
  */
 
+/* @Import
+@Component가(혹은 @Component가 붙은 어노테이션) 붙은 클래스들을 직접 추가 할 수 있음
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Configuration // 적용할 메타 애노태이션
 @ComponentScan // // 적용할 메타 애노태이션
+@Import(Config.class) // 스캔 대상 아니지만 직접 등록
 public @interface MySpringBootAnnotation {
 }

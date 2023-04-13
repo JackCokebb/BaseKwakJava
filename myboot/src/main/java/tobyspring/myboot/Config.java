17c02df1533f35758fcd1a30b247927bcccf3636
shunyang <shunyang@smilegate.com>
2023-04-13 15:40:59 +0900

    ServletWebServerFactor, DispatcherServlet 주입 할 외부 클래스 생성

--- myboot/src/main/java/tobyspring/myboot/Config.java
+++ myboot/src/main/java/tobyspring/myboot/Config.java
@@ -1,17 +1,22 @@
 package tobyspring.myboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@MySpringBootAnnotation
-public class MybootApplication {
+@Configuration
+public class Config {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MybootApplication.class, args);
-	}
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory(){
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet(){
+        return new DispatcherServlet();
+    }
 }

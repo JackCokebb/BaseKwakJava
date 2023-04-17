0fa3a40aacb7b33cc059c518e91d6783087bc704
박소율B/SGS SEED개발팀 <soyulpark@smilegate.com>
2023-04-17 18:36:01 +0900

    feat: 애노테이션 매핑 정보 사용

--- soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
 
     // 직접 object 생성 > 생성자 파라미터로 주입 받는 코드
@@ -10,6 +15,9 @@ public class HelloController {
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
+
+    @GetMapping()
+    @ResponseBody
     public String hello(String name) {
         // null 인지 체크해서 null 이면 예외를 던져줌
         return helloService.sayHello(Objects.requireNonNull(name));

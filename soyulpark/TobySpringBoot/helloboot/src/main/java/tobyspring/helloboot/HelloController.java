12f69f4b053c2c1c62debdfc5e8c9d839e88cf5c
박소율B/SGS SEED개발팀 <soyulpark@smilegate.com>
2023-04-17 18:21:28 +0900

    feat: 의존 오브젝트 추가

--- soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,11 @@
 package tobyspring.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
+    SimpleHelloService helloService = new SimpleHelloService();
     public String hello(String name) {
-        return "Hello " + name;
+        // null 인지 체크해서 null 이면 예외를 던져줌
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }

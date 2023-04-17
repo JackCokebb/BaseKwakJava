e84e4a911c8ee7167bc93e19a3dee77f901d2dc8
jhyonjee <jhyonjee@smilegate.com>
2023-04-17 17:58:26 +0900

    Section6: 자동 구성 애노테이션 적용

--- jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/MyAutoConfiguration.java
+++ jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/MyAutoConfiguration.java
@@ -9,10 +9,16 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration  // 1. 이거 사용
+// 1. @Configuration을 메타 애너테이션으로 가지고 있으니 기존에 @Configuration을 붙이고 있던 애들한테 붙여줄 수 있겠지?
+// 4. 대신 @Configuration의 element 값을 변경해 줘야함
+//      proxyBeanMethods = false -> @MyAutoConfiguration이 붙어서 동적으로 로딩되는 config들은 기존에 썼던것처럼 그냥 @Configuration이 적용된것이 아니라
+//                                  proxyBeanMethods를 false로 바꾼 Configuration이 적용된다는  -> 근데 이게 뭔데??
+@Configuration(proxyBeanMethods = false)
 public @interface MyAutoConfiguration {
-    /*
-    2. 이 애너테이션 이름으로된 설정 파일을 하나 만들자 -> 이 안에 자동 구성 방식에 사용할 config class의 목록을 넣을거임
-    3. MyAutoConfigImportSelector 코드 수정
-     */
+
 }
+// 3. ...MyAutoConfiguration.imports 파일에 있는 config 파일들에는 @MyAutoConfiguration를 붙여주는것이 원칙 -> config class로 이동
+
+
+// 5. @Configuration이 붙은 클래스의 독특한 특징에 대해 이해할 필요가 있다
+//      우리가 직접 config class를 애플리케이션 코드에 넣고 원하는 빈을 등록할 일이 많다
--- jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/autoConfig/DispatcherServletConfig.java
+++ jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/autoConfig/DispatcherServletConfig.java
@@ -3,8 +3,10 @@ package tobyspring.config.autoConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.MyAutoConfiguration;
 
-@Configuration
+//@Configuration
+@MyAutoConfiguration // 2. 이 안에도 @Configuration이 있으니 일반적인 config file로 동작할거임
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet(){

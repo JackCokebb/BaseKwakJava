7451f2da7035e7538b1f542309c3f73af8b926dd
박소율B/SGS SEED개발팀 <soyulpark@smilegate.com>
2023-04-18 10:27:35 +0900

    feat: 자바 코드 구성 정보 사용

--- soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,24 +3,31 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+// 구성 정보를 가지고 있는 클래스라는 것을 알려줘야함 > 스프링 컨테이너가 보고 이 안에 bean을 정의하는 factory method가 있겠구나 하고 bean object를 만듬
+@Configuration
 public class HellobootApplication {
 
+	// factory method 만들기 + 스프링 컨테이너에게 bean object라는 것을 알려주기 위해 bean annotation을 달아줌
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	// return 타입은 이 bean을 주입 받을 클래스에서 어떤 타입을 기대하는지에 대한 것을 적어주는 것이 좋음
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
+
 	public static void main(String[] args) {
-		// 스프링 컨테이너 생성 Applicationcontext : application을 구성하는 정보(구성 빈, 리소스 접근 방법, 내부 이벤트 전달/구독 등) 이게 스프링 컨테이너가 된다
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		// annotation 이 붙은 자바 코드를 사용하여 구성 정보를 읽어옴
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -35,8 +42,7 @@ public class HellobootApplication {
 			}
 		};
 
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(HellobootApplication.class); // 구성 정보를 가진 클래스를 등록해줘야 함
 		applicationContext.refresh();
 
 	}

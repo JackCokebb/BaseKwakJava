6243e8c6d0749351d50a496f2b2a0d7d938beed6
박소율B/SGS SEED개발팀 <soyulpark@smilegate.com>
2023-04-18 17:42:04 +0900

    feat: 자동 구성 정보 파일 분리

--- soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/config/MyAutoConfiguration.java
+++ soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/config/MyAutoConfiguration.java
@@ -1,6 +1,6 @@
 package tobyspring.config;
 
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Configuration;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import(MyAutoConfigImportSelector.class)            // 이렇게 넘겨주면 component scan 대상은 아니지만 구성 정보에 추가됨
-public @interface EnableMyAutoConfiguration {
+@Configuration
+public @interface MyAutoConfiguration {
 }

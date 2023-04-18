861a631ec62d52f1f3bc970c7b151e009071d21b
박소율B/SGS SEED개발팀 <soyulpark@smilegate.com>
2023-04-18 18:00:19 +0900

    feat: Configuration과 proxyBeanMethods

---
+++ soyulpark/TobySpringBoot/helloboot/src/test/java/tobyspring/study/ConfigurationTest.java
@@ -0,0 +1,85 @@
+package tobyspring.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ConfigurationTest {
+
+
+    // bean1과 bean2의 common은 다른 object여야 하는데 스프링 컨테이너를 이용하여 구성 정보를 등록하면 같은 object라고 나옴
+    @Test
+    void configuration() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+        ac.register(MyConfig.class);
+        ac.refresh();
+
+        Bean1 bean1 = ac.getBean(Bean1.class);
+        Bean2 bean2 = ac.getBean(Bean2.class);
+
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    // 스프링 컨테이너처럼 proxy object를 이용하면 동일한 object라고 나옴
+    @Test
+    void proxyCommonMethod() {
+        MyConfigProxy myConfigProxy = new MyConfigProxy();
+
+        Bean1 bean1 = myConfigProxy.bean1();
+        Bean2 bean2 = myConfigProxy.bean2();
+
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    static class MyConfigProxy extends MyConfig {
+        private Common common;
+
+        @Override
+        Common common() {
+            if (this.common == null) this.common = super.common();
+
+            return this.common;
+        }
+    }
+
+    @Configuration              // (proxyBeanMethods = false) 설정을 하면 proxy object를 만들지 않기 때문에 테스트가 실패함
+    static class MyConfig {
+        @Bean
+        Common common() {
+            return new Common();
+        }
+
+        @Bean
+        Bean1 bean1() {
+            return new Bean1(common());
+        }
+
+        @Bean
+        Bean2 bean2() {
+            return new Bean2(common());
+        }
+    }
+
+    static class Bean1 {
+        private final Common common;
+
+        Bean1(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Bean2 {
+        private final Common common;
+
+        Bean2(Common common) {
+            this.common = common;
+        }
+    }
+
+
+    static class Common {
+    }
+
+}

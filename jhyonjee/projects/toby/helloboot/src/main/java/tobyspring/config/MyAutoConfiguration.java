3be3e308e824942b18f257a8d71245f3530ee51a
jhyonjee <jhyonjee@smilegate.com>
2023-04-18 17:28:51 +0900

    Section6: @Configuration과 proxyBeanMethods

--- jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/MyAutoConfiguration.java
+++ jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/MyAutoConfiguration.java
@@ -9,8 +9,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-// 12. proxyBeanMethods = false로 했다는 것은 이 MyAutoConfiguration 밑에 있는 config들은 proxy로 만들어지지 않고 사용된다는 뜻
-//     @Configuration 안에 빈이 생성될때 주입되어야 할것이 업으면 주입할 오브젝트가 하나의 객체로 재사용되는지 확인할 필요 없게되고, 그럼 그걸 확인해주던 프록시도 필요 없게되는 것
 @Configuration(proxyBeanMethods = false)
 public @interface MyAutoConfiguration {
 

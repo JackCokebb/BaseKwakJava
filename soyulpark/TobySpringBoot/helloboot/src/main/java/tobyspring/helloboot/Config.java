06a8aa2f47842f3de2f5b96466037ba86573b6de
박소율B/SGS SEED개발팀 <soyulpark@smilegate.com>
2023-04-18 15:27:48 +0900

    feat: 합성 애노테이션 적용

---
+++ soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/Config.java
@@ -0,0 +1,21 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration      // component scanner 가 스캔하여 bean으로 등록
+public class Config {
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+}
--- soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ soyulpark/TobySpringBoot/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,30 +1,13 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.boot.SpringApplication;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class HellobootApplication {
-
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory() {
-		return new TomcatServletWebServerFactory();
-	}
-
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
 
 	public static void main(String[] args) {
 
-		// SpringApplication.run(HellobootApplication.class, args); 과 동일
-		MySpringApplication.run(HellobootApplication.class, args);
+		SpringApplication.run(HellobootApplication.class, args);
 	}
 
 }

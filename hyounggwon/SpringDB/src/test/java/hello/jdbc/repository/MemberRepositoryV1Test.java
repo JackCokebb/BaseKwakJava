195c49fce8be382cef440727ae6f4d3b78b1d057
SMILEGATE\hyounggwon <hyounggwon@smilegate.com>
2023-04-20 15:12:48 +0900

    섹션2: DataSource 적용 _ 기본 DriverMangager - 항상 새로운 커넥션을 획득 테스트

---
+++ hyounggwon/SpringDB/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,52 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryV1Test {
+    MemberRepositoryV1 repository;
+
+    @BeforeEach //각 테스트 실행 직전
+    void beforeEach(){
+        //기본 DriverMangager - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+
+    @Test
+    void crud() throws SQLException { //테스트는 반복해서 할 수 있는게 중요 (지금 코드는 중간에 에러 터지면 못함, 추후 트랜잭션 활용하여 수정)
+        //save
+        Member member = new Member("memberV6", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member==findMember={}", member==findMember); // false,
+        log.info("member equals findMember={}", member.equals(findMember)); // true
+        Assertions.assertThat(findMember).isEqualTo(member); // equals 이용해 비교
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+}

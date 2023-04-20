d3c763f9f28b8023a6f3c8407549dd6e39fba5b4
SMILEGATE\hyounggwon <hyounggwon@smilegate.com>
2023-04-20 10:18:20 +0900

    섹션1. JDBC 개발 - 삭제

--- hyounggwon/SpringDB/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ hyounggwon/SpringDB/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,9 +16,9 @@ class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
-    void crud() throws SQLException {
+    void crud() throws SQLException { //테스트는 반복해서 할 수 있는게 중요 (지금 코드는 중간에 에러 터지면 못함, 추후 트랜잭션 활용하여 수정)
         //save
-        Member member = new Member("memberV5", 10000);
+        Member member = new Member("memberV6", 10000);
         repository.save(member);
 
         //findById
@@ -31,5 +32,10 @@ class MemberRepositoryV0Test {
         repository.update(member.getMemberId(), 20000);
         Member updatedMember = repository.findById(member.getMemberId());
         assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        Assertions.assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }

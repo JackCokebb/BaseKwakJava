fdc9de8591e75ff87e90609b0a85c7ff25ec595e
jhyonjee <jhyonjee@smilegate.com>
2023-04-25 16:03:16 +0900

    Section 4: 스프링 부트의 자동 리소스 등록

--- jhyonjee/projects/SpringDB1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
+++ jhyonjee/projects/SpringDB1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_4Test.java
@@ -5,7 +5,6 @@ import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -13,23 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
-import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- * 트랜잭션 - @Transactional AOP
+ * 트랜잭션 - 이번에는 스프링 부트가 제공하는 자동 등록을 이용해서 데이터소스와 트랜잭션매니저를 편리하게 적용해보자
  */
 @Slf4j
 @SpringBootTest
-class MemberServiceV3_3Test {
+class MemberServiceV3_4Test {
 
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
@@ -42,19 +37,30 @@ class MemberServiceV3_3Test {
 
     @TestConfiguration
     static class TestConfig{
-        @Bean
-        DataSource dataSource(){
-            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-        }
+        // 1. 이번에는 스프링 부트가 제공하는 자동 등록을 이용해서 데이터소스와 트랜잭션매니저를 편리하게 적용해보자
 
-        @Bean
-        PlatformTransactionManager transactionManager(){
-            return new DataSourceTransactionManager(dataSource());
+//        @Bean
+//        DataSource dataSource(){
+//            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+//        }
+//
+//        @Bean
+//        PlatformTransactionManager transactionManager(){
+//            return new DataSourceTransactionManager(dataSource());
+//        }
+
+        //2. memberRepository에 필요한 dataSource는 어케 주입하지??
+            // @Autowire 가능
+            // 생성자 주입 가능
+        private final DataSource dataSource;
+
+        public TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
         }
 
         @Bean
         MemberRepositoryV3 memberRepositoryV3(){
-            return new MemberRepositoryV3(dataSource());
+            return new MemberRepositoryV3(dataSource);
 
         }
         @Bean
@@ -122,3 +128,4 @@ class MemberServiceV3_3Test {
     }
 
 }
+// 3. 그냥 돌리면 메모리에 있는 DB를 쓰려고 하기때문에 application.properties에 설정해줘야함

43d58205386ca3221600e7555d9e407a883297b5
jhyonjee <jhyonjee@smilegate.com>
2023-04-26 19:36:08 +0900

    Section 5: 예외 포함과 스택 트레이스

--- jhyonjee/projects/SpringDB1/jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ jhyonjee/projects/SpringDB1/jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,11 +1,13 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
 import java.sql.SQLException;
 
+@Slf4j
 public class UnCheckedAppTest {
     @Test
     void unchecked(){
@@ -13,11 +15,22 @@ public class UnCheckedAppTest {
         Assertions.assertThatThrownBy(()-> controller.request())
                 .isInstanceOf(Exception.class);
     }
+    @Test
+    void printEx(){
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        }
+        catch (Exception e){
+//            e.printStackTrace(); -> 좋지 않은 방식
+            log.info("# ex", e); // 지금 예에서는 파라미터 (ex. class={})가 없기 때문에, 예외(e)만 파라미터에 전달하면 스택 트레이스를 로그에 출력할 수 있다.
+        }
+    }
 
     static class Controller{
         Service service = new Service();
 
-        public void request() throws SQLException, ConnectException {
+        public void request(){
             service.logic();
         }
     }
@@ -25,13 +38,13 @@ public class UnCheckedAppTest {
         Repository repository = new Repository();
         NetworkClient networkClient = new NetworkClient();
 
-        public void logic(){ // throws SQLException, ConnectException { -> 이제 이 예외들 다 뗼수있지
+        public void logic(){
             repository.call();
             networkClient.call();
         }
     }
     static class NetworkClient{
-        public void call(){ // throws ConnectException { -> 이제 이 예외들 다 뗼수있지
+        public void call(){
             throw new RuntimeConnectException("# conn fail ex");
         }
 
@@ -39,25 +52,27 @@ public class UnCheckedAppTest {
     static class Repository{
         public void call(){
             try {
-                runSQL(); // 3. repository 내부적으로 일단 runSQL 실행 -> 근데 여기서 이 체크 예외 잡을 것임 -> 잡아서 밖으로 던질때는 런타임 에러(언체크)로 바꿔서 던짐
+                runSQL();
             } catch (SQLException e) {
-                throw new RuntimeSQLException(e); // 원래 error도 넣어줘야 원래 예외의 stacktrace도 포함해서 추적가능
+                throw new RuntimeSQLException(e); // 기존 예외(e) 포함 -> caused by .. 로 같이 찍힘 -> 기존 예외 빠뜨리지 말자
             }
 
         }
-        public void runSQL() throws SQLException { // 2. SQL을 실행한다고 가정
-            throw new SQLException("# ex"); // check 예외 터짐
+        public void runSQL() throws SQLException {
+            throw new SQLException("# ex");
         }
     }
-    static class RuntimeConnectException extends RuntimeException{ // 1. uncheck 에외 정의
+    static class RuntimeConnectException extends RuntimeException{
         public RuntimeConnectException(String message) {
             super(message);
         }
     }
 
-    static class  RuntimeSQLException extends RuntimeException{  // 1. uncheck 에외 정의
+    static class  RuntimeSQLException extends RuntimeException{
+        public RuntimeSQLException() { // 기존 예외 미포함
+        }
 
-        public RuntimeSQLException(Throwable cause) {
+        public RuntimeSQLException(Throwable cause) { // 기존 예외 포함
             super(cause);
         }
     }

9c7bcbc95a0493fe11a82e38af391725bc7f3fd2
jhyonjee <jhyonjee@smilegate.com>
2023-04-28 08:16:11 +0900

    Section 6: 정리

--- jhyonjee/projects/SpringDB1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV5.java
+++ jhyonjee/projects/SpringDB1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV5.java
@@ -4,17 +4,10 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.datasource.DataSourceUtils;
-import org.springframework.jdbc.support.JdbcUtils;
-import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
-import org.springframework.jdbc.support.SQLExceptionTranslator;
-
 import javax.sql.DataSource;
-import java.sql.*;
-import java.util.NoSuchElementException;
 
 /**
- * JdbcTempalte 사용
+ * JdbcTempalte 사용 - 리포지토리에서 JDBC를 사용하는 반복 코드가 제거됨.
  */
 @Slf4j
 public class MemberRepositoryV5 implements MemberRepository{
@@ -27,70 +20,16 @@ public class MemberRepositoryV5 implements MemberRepository{
     @Override
     public Member save(Member member){
         String sql = "INSERT INTO member(member_id, money) VALUES (?, ?)";
-        int updatedRows = jdbcTemplate.update(sql, member.getMemberId(), member.getMoney());// preparedStatement.executeUpdate(); 여서 update임. -> update된 row 수 반환
-        // member.getMemberId(), member.getMoney()는 각 ?에 들어가는 파라미터.
+        int updatedRows = jdbcTemplate.update(sql, member.getMemberId(), member.getMoney());
         return member;
-
-
-//          JdbcTemplate이 다 해주기 때문에 JdbcTemplate으로 밑에 중복되는 코드 제거 가능!(connection 받는 거부터 ~ 예외 변환까지)
-//        Connection con = null;
-//        PreparedStatement preparedStatement = null;
-//
-//
-//        try {
-//            con = getConnection();
-//            preparedStatement = con.prepareStatement(sql);
-//
-//            preparedStatement.setString(1, member.getMemberId());
-//            preparedStatement.setInt(2, member.getMoney());
-//            preparedStatement.executeUpdate();
-//
-//            return member;
-//        } catch (SQLException e) {
-//            throw exTranslator.translate("save", sql, e);
-//        }
-//        finally {
-//            close(con, preparedStatement, null);
-//
-//        }
     }
 
     @Override
     public Member findById(String memberId){
         String sql = "SELECT * FROM MEMBER WHERE member_id = ?";
 
-        // 한 건만 조회하는 것은 queryForObject
-        Member member = jdbcTemplate.queryForObject(sql, memberRowMapper(), memberId);// memberRowMapper: query 결과를 어떻게 매핑할 것인지에 대한 정보
+        Member member = jdbcTemplate.queryForObject(sql, memberRowMapper(), memberId);
         return member;
-
-        /*Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        
-        try{
-            connection = getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, memberId);
-
-            resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
-                Member member = new Member();
-                member.setMemberId(resultSet.getString("member_id"));
-                member.setMoney(resultSet.getInt("money"));
-                return member;
-            }
-            else{
-                throw new NoSuchElementException("# member not found memberId = " + memberId);
-            }
-
-        } catch (SQLException e) {
-            log.error("# prepareStatement ERROR - findById()");
-            throw exTranslator.translate("findById", sql, e);
-        }
-        finally {
-            close(connection, preparedStatement, resultSet);
-        }*/
-
     }
 
     private RowMapper<Member> memberRowMapper() {
@@ -107,67 +46,11 @@ public class MemberRepositoryV5 implements MemberRepository{
     public void update(String memberId, int money){
         String sql = "UPDATE member SET money=? WHERE member_id=?";
         jdbcTemplate.update(sql, money, memberId);
-
-
-        /*Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        try{
-            connection = getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, money);
-            preparedStatement.setString(2, memberId);
-
-            int resultSize = preparedStatement.executeUpdate();
-            log.info("# result size = {}", resultSize);
-
-        } catch (SQLException e) {
-            log.error("# prepareStatement ERROR - update()");
-            throw exTranslator.translate("update", sql, e);
-        }
-        finally {
-            close(connection, preparedStatement, null);
-        }*/
     }
 
     @Override
     public void delete(String memberId){
         String sql = "DELETE FROM member WHERE member_id=?";
         jdbcTemplate.update(sql,memberId);
-
-        /*Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        try{
-            connection = getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, memberId);
-
-            int resultSize = preparedStatement.executeUpdate();
-
-        } catch (SQLException e) {
-            log.error("# prepareStatement ERROR - delete()");
-            throw exTranslator.translate("delete", sql, e);
-            //throw new MyDbException(e);
-        }
-        finally {
-            close(connection, preparedStatement, null);
-        }*/
     }
-
-    /*
-    // Connection 열고 닫는것도 JdbcTemplate에서 다 해줌!!
-    private void close(Connection con, Statement statement, ResultSet resultSet){
-
-        JdbcUtils.closeResultSet(resultSet);
-        JdbcUtils.closeStatement(statement);
-        DataSourceUtils.releaseConnection(con, dataSource);
-    }
-
-    private Connection getConnection(){
-        Connection connection = DataSourceUtils.getConnection(dataSource);
-        log.info("# Get connection={}, class={} v1", connection, connection.getClass());
-        return connection;
-    }
-    */
 }
--- jhyonjee/projects/SpringDB1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV4.java
+++ jhyonjee/projects/SpringDB1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -7,29 +7,29 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 /**
- * 예외 누수 문제 해결
- * SQLException 제거
- *
- * MemberRepository 인터페이스 의존
- * 이제 거의 순수 자바 코드만 남을 것
+ * 서비스 계층의 순수성
+ *  트랜잭션 추상화 + 트랜잭션 AOP 덕분에 서비스 계층의 순수성을 최대한 유지하면서 서비스 계층에서 트랜잭션을 사용할 수 있다.
+ *  스프링이 제공하는 예외 추상화와 예외 변환기 덕분에, 데이터 접근 기술이 변경되어도 서비스 계층의 순수성을 유지하면서 예외도 사용할 수 있다.
+ *  서비스 계층이 리포지토리 인터페이스에 의존한 덕분에 향후 리포지토리가 다른 구현 기술로 변경되어도 서비스 계층을 순수하게 유지할 수 있다.
  */
 
 @Slf4j
 public class MemberServiceV4 {
 
-    private final MemberRepository memberRepository; // 1. repository 인터페이스로 교체
+    private final MemberRepository memberRepository;
 
-    public MemberServiceV4(MemberRepository memberRepository) { // 2. 주입도 인터페이스로
+    public MemberServiceV4(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
 
     }
 
+    // 깔끔하게 비즈니스 로직만 트랜잭션을 적용할 수 있게 됨.
     @Transactional
     public void accountTransfer(String fromId, String toId, int money) { //throws SQLException { -> 3. 예외 명시 제거
             bizLogic(fromId, toId, money);
     }
 
-    private void bizLogic(String fromId, String toId, int money) {  //throws SQLException { -> 3. 예외 명시 제거
+    private void bizLogic(String fromId, String toId, int money) {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
 

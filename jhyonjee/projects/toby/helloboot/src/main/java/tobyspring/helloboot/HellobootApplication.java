e49ddd127dfb3bec8374fc2ca197dfb9c131c6ae
JackCokebb <reo2726@gmail.com>
2023-05-11 00:24:15 +0900

    Section7 : 스프링 부트의 @Conditional

--- jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,14 +1,16 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import tobyspring.config.MySpringBootApplication;
-
 
 @MySpringBootApplication
 public class HellobootApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
 	}
-
 
 }

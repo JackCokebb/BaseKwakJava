194b271c4c713989acd8a914f502c95de5ffc858
JackCokebb <reo2726@gmail.com>
2023-05-10 23:13:15 +0900

    Section7 : 커스텀 @Conditional

--- jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/autoConfig/JettyWebServerConfig.java
+++ jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/autoConfig/JettyWebServerConfig.java
@@ -1,38 +1,22 @@
-package tobyspring.config.autoConfig;
+package tobyspring.config.autoconfig;
 
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
-/**
- * 조건을 부여해서 특정 조건이 만족되면 jetty server를 쓰게끔 설정해보자 -> 선택이 가능하게끔
- */
 @MyAutoConfiguration
-//@configuration -> 클래스 레벨에 붙이는 것
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
-    public ServletWebServerFactory servletWebServerFactory(){
+    public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
-
-    // 한 파일에서 보기 편하게 중첩 static class로 생성
-    // Condition ineterface를 구현한 class
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            // JettyWebServerConfig class를 빈으로 등록할 것인지 결정하는 것에 대한 정보를 spring container에게 알려줌
-            // ConditionContext: 현재 이 컨테이너와 애플리케이션이 돌아가고 있는 환경에 대해 알 수 있음
-            // AnnotatedTypeMetadata: @Conditional과 함꼐 붙은 애너테이션들의 정보를 알려주는 메타데이터
-
-            return true; // true를 리턴하면 이 구성정보를 사용하겠다는 뜻!
-        }
-    }
 }
-
-
--- jhyonjee/projects/toby/helloboot/src/test/java/tobyspring/study/ConditionalTest.java
+++ jhyonjee/projects/toby/helloboot/src/test/java/tobyspring/study/ConditionalTest.java
@@ -1,6 +1,5 @@
 package tobyspring.study;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.*;
@@ -16,56 +15,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConditionalTest {
     @Test
-    void conditional(){
-        ApplicationContextRunner applicationContextRunner = new ApplicationContextRunner();
-        applicationContextRunner.withUserConfiguration(Config1.class)
+    void conditional() {
+        // true
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config1.class)
                 .run(context -> {
                     assertThat(context).hasSingleBean(MyBean.class);
                     assertThat(context).hasSingleBean(Config1.class);
                 });
 
+        // false
         new ApplicationContextRunner().withUserConfiguration(Config2.class)
                 .run(context -> {
                     assertThat(context).doesNotHaveBean(MyBean.class);
-                    assertThat(context).doesNotHaveBean(Config2.class);
+                    assertThat(context).doesNotHaveBean(Config1.class);
                 });
     }
 
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.TYPE)
     @Conditional(BooleanCondition.class)
-    @interface BooleanConditional{
-        boolean value(); // single element만 사용하고자 할 때 사용 가능
-    }  // annotation의 element값을 지정할 수 있도록 만들어 보자
+    @interface BooleanConditional {
+        boolean value();
+    }
 
     @Configuration
     @BooleanConditional(true)
-    static class Config1{
+    static class Config1 {
         @Bean
-        MyBean myBean(){
+        MyBean myBean() {
             return new MyBean();
         }
     }
-
 
     @Configuration
     @BooleanConditional(false)
-    static class Config2{
+    static class Config2 {
         @Bean
-        MyBean myBean(){
+        MyBean myBean() {
             return new MyBean();
         }
     }
 
-    static class MyBean{
+    static class MyBean {}
 
-    }
-
-    private static class BooleanCondition implements Condition {
+    static class BooleanCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            // @BooleanConditional(false) 처럼 조건 (ex. false)에 따라 처리되도록 만들자
-            Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(BooleanConditional.class.getName());// Annotation에 붙어있는 elements == attributes를 다 가져오는 것
+            Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(BooleanConditional.class.getName());
             Boolean value = (Boolean)annotationAttributes.get("value");
             return value;
         }

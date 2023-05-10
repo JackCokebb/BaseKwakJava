4b7908e4f01aee43efbf81919e2e1b4b6b2b99a1
JackCokebb <reo2726@gmail.com>
2023-05-11 00:14:49 +0900

    Section7 : 자동 구성 정 대체하기1

--- jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/autoConfig/TomcatWebServerConfig.java
+++ jhyonjee/projects/toby/helloboot/src/main/java/tobyspring/config/autoConfig/TomcatWebServerConfig.java
@@ -1,9 +1,9 @@
 package tobyspring.config.autoConfig;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
@@ -12,6 +12,7 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
